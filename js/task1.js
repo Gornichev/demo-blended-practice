203c02387a687c8b0f08b4589c28438fa9a5bd7e
Gornichev <gornichev1986@gmail.com.noreply.github.com>
2022-04-26 19:17:44 +0300

    // task Polindrom

--- js/task1.js
+++ js/task1.js
@@ -1,3 +1,12 @@
-/*
- * Перевірити, чи є число цілим
- */
+ // task Polindrom
+function palindrome (str){
+  str = str.toLowerCase()
+  return str === str.split("").reverse().join("")
+
+}
+console.log(palindrome('aB2brgrta'))
+console.log(palindrome('aBba'))
+console.log(palindrome('abba'))
+console.log(palindrome('AbbA'))
+ // task Polindrom
+
