0f398760e051e0bafed39b46426e77be2e65d277
Gornichev <gornichev1986@gmail.com.noreply.github.com>
2022-04-27 19:14:19 +0300

    // task : Search for an item              // Write a script to find the smallest number in the array.

--- js/task1.js
+++ js/task1.js
@@ -1,12 +1,64 @@
  // task Polindrom
-function palindrome (str){
-  str = str.toLowerCase()
-  return str === str.split("").reverse().join("")
-
-}
-console.log(palindrome('aB2brgrta'))
-console.log(palindrome('aBba'))
-console.log(palindrome('abba'))
-console.log(palindrome('AbbA'))
+// function palindrome (str){
+//   str = str.toLowerCase().trim();
+//   return str === str.split("").reverse().join("")
+//
+// }
+// console.log(palindrome('aB2brgrta'))
+// console.log(palindrome('aBba'))
+// console.log(palindrome('abba'))
+// console.log(palindrome('AbbA'))
  // task Polindrom
 
+
+// / Приклад 1 -
+//  // Напиши скрипт для перебору масиву fruits циклом for.
+//  // Для кожного елемента масиву виведіть у консоль рядок у форматі "номер_елемента: значення_елемента".
+//  // Нумерація елементів має починатися з 1
+
+//  const mooFruits = ['🍎', '🍇', '🍑', '🍌', '🍋'];
+//
+// for (let i = 0; i < mooFruits.length; i += 1){
+//   console.log(`{i + 1} : ${mooFruits[i]}`)
+// }
+
+
+ // / Приклад 2 - Переписати на for of
+ // // Скрипт виводить у консоль лише парні елементи масиву.
+
+// let numbers = [5, 10, 15, 20, 25, 30, 35, 40, 48, 51];
+
+//
+// for (let i =0; i <= numbers.length; i += 1){
+//
+// if (numbers[i] % 2 !== 0){
+// continue;
+// }
+//  console.log(numbers[i])
+// }
+
+//
+// for (const number of numbers){
+//   if (number % 2 !== 0){
+//     continue;
+//   }
+//   if (number > 30){
+//     break;
+//   }
+//   console.log(number)
+// }
+
+ // Приклад 3 - Пошук елемента
+ // Напиши скрипт пошуку найменшого числа у масиві.
+ // Код має працювати для будь - якого масиву чисел.
+ // Використовуйте цикл для вирішення задачі.
+
+//  const numbers = [5, 2, 17, 94, 1, 23, 37, 0.5];
+//  let min = numbers[0];
+//
+// for (const number of numbers){
+//   if (number < min){
+//     min = number
+//   }
+//   console.log('min', min)
+// }
