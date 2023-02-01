b1637bf2a03a5b398c70993a0ad2c93527e275ec
Gornichev <gornichev1986@gmail.com.noreply.github.com>
2023-02-01 10:38:26 +0200

    Go It : for/of

--- js/task1.js
+++ js/task1.js
@@ -680,17 +680,48 @@
 // }
 
 
-function checkStorage(available, ordered) {
+// function checkStorage(available, ordered) {
+//   // Change code below this line
+//
+//   if (ordered === 0) {
+//     return  "Your order is empty!";
+//
+//   } if (ordered > available) {
+//     return "Your order is too large, not enough goods in stock!";
+//   }
+//     return  "The order is accepted, our manager will contact you";
+//
+// }
+// console.log(checkStorage(100, 130))
+//
+
+// const fruits = ["apple", "peach", "pear", "banana"];
+//
+// // Change code below this line
+// const lastElementIndex = fruits.length -1;
+// const lastElement = fruits[lastElementIndex];
+//
+// console.log(lastElementIndex)
+// console.log(lastElement)
+
+// function getExtremeElements(array) {
+//
+//   return [array[0],array[array.length -1]]
+//
+// }
+//
+// console.log(getExtremeElements([1, 2, 3, 4, 5]))
+// const fruits = "Mango and other fruits"
+// const stringToArray = fruits.split(' ')
+//
+// console.log(stringToArray)
+function calculateEngravingPrice(message, pricePerWord) {
   // Change code below this line
 
-  if (ordered === 0) {
-    return  "Your order is empty!";
-
-  } if (ordered > available) {
-    return "Your order is too large, not enough goods in stock!";
-  }
-    return  "The order is accepted, our manager will contact you";
+  return message.split(' ').length * pricePerWord;
 
 }
-console.log(checkStorage(100, 130))
+
+console.log(calculateEngravingPrice("JavaScript is in my blood", 10))
+
 
